3670350c966bb2dac391fe4d6cf24caa276fc78e
Pian <alifalpian157@gmail.com>
2025-08-01 18:48:15 +0700

    test

--- app/about/index.tsx
+++ app/about/index.tsx
@@ -2,7 +2,7 @@ export default function AboutPage() {
    return (
       <div>
          <h1>About Page</h1>
-         <p>This is the about page of our Next.js application.</p>
+         <p>This is the about page of our Next.js application</p>
       </div>
    )
 }
