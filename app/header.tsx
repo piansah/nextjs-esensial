fd32b7362eb616caf873bac745b53ea4d1da09a1
Pian <alifalpian157@gmail.com>
2025-08-09 20:52:20 +0700

    react hook data fetching dalam client

--- app/header.tsx
+++ app/header.tsx
@@ -1,18 +1,12 @@
-import Link from "next/link";
-import {getSetting} from "@/app/queries/getSetting";
+import { getSetting } from "@/app/queries/getSetting";
+import { Nav } from "./nav";
 
 export async function Header() {
   const setting = await getSetting();
   return (
     <header className="border-b border-white py-2 mb-2">
-      <nav className="space-x-4 p-4">
-        <div className="text-2xl">{setting.siteName}</div>
-        <Link href="/">Home</Link>
-        <Link href="/about">About</Link>
-        <Link href="/about/team">Team</Link>
-        <Link href="/admin">Admin</Link>
-        <Link href="/posts">Posts</Link>
-      </nav>
+      <div className="text-2xl">{setting.siteName}</div>
+      <Nav />
     </header>
   );
 }
---
+++ app/nav.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import Link from "next/link";
+import { Setting } from "./types/setting";
+import { use } from "react";
+
+export function Nav({settingPromise}:{settingPromise:Promise<Setting>}) {
+  const setting = use(settingPromise);
+
+  console.log("Nav setting", setting);
+
+  return (
+    <nav className="space-x-4 p-4">
+      <Link href="/">Home</Link>
+      <Link href="/about">About</Link>
+      <Link href="/about/team">Team</Link>
+      <Link href="/admin">Admin</Link>
+      <Link href="/posts">Posts</Link>
+    </nav>
+  );
+}
