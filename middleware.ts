7e4c993473dbee122ebe7bc169fb7a9b31fdd3fd
Pian <alifalpian157@gmail.com>
2025-08-12 12:41:12 +0700

    mengambil cookies

--- middleware.ts
+++ middleware.ts
@@ -2,10 +2,14 @@ import { NextResponse, type NextRequest } from "next/server";
 
 export function middleware(request: NextRequest) {
   const { pathname } = request.nextUrl;
+  const token = request.cookies.get("token");
+
+  console.log("Token:", token);
 
   if (pathname.startsWith("/admin")) {
-    // return NextResponse.rewrite(new URL("/forbidden", request.url));
-    return NextResponse.redirect(new URL("/forbidden", request.url));
-
+    return NextResponse.rewrite(new URL("/forbidden", request.url));
   }
+  // if (pathname.startsWith("/about")) {
+  //   return NextResponse.redirect(new URL("/forbidden", request.url));
+  // }
 }
