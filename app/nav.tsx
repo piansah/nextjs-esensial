78a39cf1f8c29de118a52b88c605e6af95682b15
Pian <alifalpian157@gmail.com>
2025-08-12 12:30:30 +0700

    rewrite&redirect middleware

--- app/nav.tsx
+++ app/nav.tsx
@@ -5,10 +5,11 @@ export function Nav() {
   return (
     <nav className="space-x-4 p-4">
       <Link href="/">Home</Link>
-      <Link href="/about">About</Link>
       <Link href="/about/team">Team</Link>
-      <Link href="/admin">Admin</Link>
+      <Link href="/forbidden">Forbidden</Link>
       <Link href="/posts">Posts</Link>
+      <Link href="/about">About</Link>
+      <Link href="/admin">Admin</Link>
       <Link href="/api/user">User API</Link>
     </nav>
   );
--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,11 @@
-import type { NextRequest } from "next/server";
+import { NextResponse, type NextRequest } from "next/server";
 
 export function middleware(request: NextRequest) {
   const { pathname } = request.nextUrl;
 
   if (pathname.startsWith("/admin")) {
-    console.log("Admin Page");
+    // return NextResponse.rewrite(new URL("/forbidden", request.url));
+    return NextResponse.redirect(new URL("/forbidden", request.url));
+
   }
 }
